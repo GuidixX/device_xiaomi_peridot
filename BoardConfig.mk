0adcc922e2a49a947949fc19d36b38d741f62075
raghavt20 <raghavtandon2000@gmail.com>
2024-07-16 13:33:03 +0530

    peridot: Set Miui Camera Package Name
    Change-Id: Ia5f4d6ac34d4cb6b1002330762ae9673b3a514f9
    Signed-off-by: Guidix <guidixm@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -182,6 +182,9 @@ TARGET_KERNEL_EXT_MODULES := \
     qcom/opensource/touch-drivers \
     nxp/opensource/driver
 
+# MiuiCamera
+CAMERA_PACKAGE_NAME := com.android.camera
+
 # Partitions
 -include vendor/voltage/config/BoardConfigReservedSize.mk
 
