9bf31c4b92934012bfe56a23d652d01980956b43
bengris32 <bengris32@protonmail.ch>
2024-10-07 23:50:11 +0100

    peridot: camera: Implement supportsSetTorchModeExt
    Change-Id: I2ec2f4a30723763e6123a1b742468752f38e3d2f
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- configs/camera/CameraProviderExtension.cpp
+++ configs/camera/CameraProviderExtension.cpp
@@ -35,6 +35,11 @@ static T get(const std::string& path, const T& def) {
 bool supportsTorchStrengthControlExt() {
     return true;
 }
+
+bool supportsSetTorchModeExt() {
+    return true;
+}
+
 int32_t getTorchDefaultStrengthLevelExt() {
     return 7;
 }
