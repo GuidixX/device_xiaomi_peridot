ffbd4a02ad0ccf2a6103532f0292b72bbc9a6c9d
lolipuru <banstellazz@gmail.com>
2025-02-10 08:30:04 +0000

    peridot: don't set fod status on cancel
    prob unnecessary
    
    Signed-off-by: lolipuru <banstellazz@gmail.com>
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- configs/udfps/UdfpsHandler.cpp
+++ configs/udfps/UdfpsHandler.cpp
@@ -84,7 +84,7 @@ class XiaomiSM8650UdfpsHander : public UdfpsHandler {
     void cancel() {
         LOG(INFO) << __func__;
         setFingerDown(false);
-        setFodStatus(FOD_STATUS_OFF);
+        //setFodStatus(FOD_STATUS_OFF);
     }
 
   private:
