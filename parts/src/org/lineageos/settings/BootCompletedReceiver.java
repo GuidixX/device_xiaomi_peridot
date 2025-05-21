a589a17daa0a52ab4de8b37b50bf1773f8324597
kenway214 <kenway214@outlook.com>
2025-05-21 18:06:47 +0530

    peridot: parts: Move DT2W logic to dedicated touch services
    Refactors the initialization logic for Double Tap to Wake feature
    into standalone services—DoubleTapService
    
    - DoubleTapService registers a content observer for the
      DOUBLE_TAP_TO_WAKE setting and applies the corresponding touch mode via
      ITouchFeature.
    
    This change simplifies BootCompletedReceiver and improves modularity by
    isolating feature-specific logic in dedicated service classes.
    
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -38,6 +38,7 @@ import org.lineageos.settings.touchsampling.TouchSamplingUtils;
 import org.lineageos.settings.touchsampling.TouchSamplingService;
 import org.lineageos.settings.touchsampling.TouchSamplingTileService;
 import org.lineageos.settings.soundcontrol.SoundControlUtils;
+import org.lineageos.settings.touch.DoubleTapService;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final String TAG = "XiaomiParts";
@@ -65,8 +66,11 @@ public class BootCompletedReceiver extends BroadcastReceiver {
             // Override HDR types
             overrideHdrTypes(context);
 
+            // Restore touch sampling rate
+            TouchSamplingUtils.restoreSamplingValue(context);
+
         } catch (Exception e) {
-            Log.e(TAG, "Error during locked boot completed processing", e);
+            Log.e(TAG, "Error during locked boot completed", e);
         }
     }
 
@@ -105,6 +109,9 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
         // Start Touch Sampling Service
         context.startServiceAsUser(new Intent(context, TouchSamplingService.class), UserHandle.CURRENT);
+
+        // Start Touchfeatures service
+        context.startServiceAsUser(new Intent(context, DoubleTapService.class), UserHandle.CURRENT);
     }
 
     private void overrideHdrTypes(Context context) {
