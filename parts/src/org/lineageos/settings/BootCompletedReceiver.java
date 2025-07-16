6a66f57bd95bfbf58fe892d6e51b2556013339e9
Guidix <guidixm@gmail.com>
2025-07-16 18:26:02 +0300

    Revert "peridot: parts: observe DOZE_DOUBLE_TAP_GESTURE and DOZE_TAP_SCREEN_GESTURE for tap gestures"
    this reverts commit d26078d4ff7f0bf2f80cdc3ebbcafd10b24bc850

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -33,7 +33,6 @@ import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.thermal.ThermalTileService;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 import org.lineageos.settings.turbocharging.TurboChargingService;
-import org.lineageos.settings.sensors.TapToWakeService;
 import org.lineageos.settings.touchsampling.TouchSamplingUtils;
 import org.lineageos.settings.touchsampling.TouchSamplingService;
 import org.lineageos.settings.touchsampling.TouchSamplingTileService;
@@ -98,15 +97,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
         // Start Touch Sampling Service
         context.startServiceAsUser(new Intent(context, TouchSamplingService.class), UserHandle.CURRENT);
-
-        // Start TapToWakeService
-        try {
-            Class<?> tapToWakeService = Class.forName("org.lineageos.settings.sensors.TapToWakeService");
-            Intent tapToWakeIntent = new Intent(context, tapToWakeService);
-            context.startServiceAsUser(tapToWakeIntent, UserHandle.CURRENT);
-        } catch (ClassNotFoundException e) {
-            Log.e(TAG, "TapToWakeService class not found", e);
-        }
     }
 
     private void overrideHdrTypes(Context context) {
