592884c79f55955edc415dce480a5a45f13f427d
AdarshGrewal <adarshgrewal@gmail.com>
2025-08-15 21:10:56 +0530

    peridot: Build libprocessgroup vendor lib
    Change-Id: I4a3f8be5bc453001f485a1499cda15e273a015e9

--- device.mk
+++ device.mk
@@ -440,6 +440,7 @@ PRODUCT_PACKAGES += \
     libcurl.vendor \
     libjson \
     libjsoncpp.vendor \
+    libprocessgroup.vendor \
     libqti_vndfwk_detect.vendor \
     libqti_vndfwk_detect_vendor \
     libsqlite.vendor \
