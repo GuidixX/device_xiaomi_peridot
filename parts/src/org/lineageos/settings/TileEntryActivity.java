ff78bd6e6996b69f4bc720b31ebe6373e18cce5b
Guidix <guidixm@gmail.com>
2025-09-22 23:48:58 +0300

    peridot: Cleanup and organize
    Cleanup parts services
    Drop sepolicy for dropped parts services
    Fix dependencies
    Drop duplicated build prop overrides
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- parts/src/org/lineageos/settings/TileEntryActivity.java
+++ parts/src/org/lineageos/settings/TileEntryActivity.java
@@ -27,13 +27,11 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 
 import org.lineageos.settings.saturation.SaturationActivity;
-import org.lineageos.settings.touchsampling.TouchSamplingSettingsActivity;
 import org.lineageos.settings.chargecontrol.ChargeControlActivity;
 
 public class TileEntryActivity extends Activity {
     private static final String TAG = "TileEntryActivity";
     private static final String SATURATION_TILE = "org.lineageos.settings.saturation.SaturationTileService";
-    private static final String HTSR_TILE = "org.lineageos.settings.touchsampling.TouchSamplingTileService";
     private static final String CHGCTRL_TILE = "org.lineageos.settings.chargecontrol.ChargeControlTileService";
 
     @Override
@@ -51,8 +49,6 @@ public class TileEntryActivity extends Activity {
 
         if (SATURATION_TILE.equals(sourceClassName)) {
             intent = new Intent(this, SaturationActivity.class);
-        } else if (HTSR_TILE.equals(sourceClassName)) {
-            intent = new Intent(this, TouchSamplingSettingsActivity.class);
         } else if (CHGCTRL_TILE.equals(sourceClassName)) {
             intent = new Intent(this, ChargeControlActivity.class);
         } else {
