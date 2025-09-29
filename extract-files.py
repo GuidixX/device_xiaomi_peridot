9a59a89112d79e5d86424ebd46e67b7ba0ff53f1
Guidix <guidixm@gmail.com>
2025-09-30 00:50:25 +0300

    peridot: Update Blobs from OS2.0.205.0.VNPMIXM
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -96,7 +96,7 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed(
             'android.hardware.graphics.allocator-V1-ndk.so',
             'android.hardware.graphics.allocator-V2-ndk.so'
-    ),
+        ),
     (
         'odm/lib64/camera/com.qti.actuator.peridot_aac_imx882_gt9764ber_wide_i_actuator.so',
         'odm/lib64/camera/com.qti.actuator.peridot_ofilm_imx882_aw86016csr_wide_ii_actuator.so',
@@ -265,7 +265,12 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libwrapper_dlengine.so'),
 
     'odm/lib64/hw/camera.xiaomi.so': blob_fixup()
-        .replace_needed('libui.so', 'libui-v34.so'),
+        .add_needed('libprocessgroup_shim.so')
+        .replace_needed('libui.so', 'libui-v34.so')
+        .replace_needed(
+            'android.hardware.graphics.allocator-V1-ndk.so',
+            'android.hardware.graphics.allocator-V2-ndk.so'
+        ),
 
     (
         'odm/lib64/camera/components/com.mi.node.mawsaliency.so',
@@ -274,7 +279,11 @@ blob_fixups: blob_fixups_user_type = {
     ): blob_fixup()
         .add_needed(
             'libwrapper_dlengine_shim.so'
-    ),
+    )
+        .replace_needed(
+            'android.hardware.graphics.allocator-V1-ndk.so',
+            'android.hardware.graphics.allocator-V2-ndk.so'
+        ),
 
     'vendor/etc/vintf/manifest/c2_manifest_vendor.xml': blob_fixup()
         .regex_replace(r'.+DOLBY.+\n', '')
