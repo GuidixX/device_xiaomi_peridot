91d02cede97441b807a39dd72dce25e6f081462f
Guidix <guidixm@gmail.com>
2025-10-02 22:52:11 +0300

    peridot: Build android.hardware.audio.sounddose-vendor-impl
    E audiohalservice: Failed to dlopen android.hardware.audio.sounddose-vendor-impl.so: dlopen failed: library "android.hardware.audio.sounddose-vendor-impl.so" not found
    W audiohalservice: createISoundDoseFactory() from android.hardware.audio.sounddose-vendor-impl failed
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- device.mk
+++ device.mk
@@ -59,7 +59,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio@7.1-impl \
-    android.hardware.soundtrigger@2.3-impl
+    android.hardware.soundtrigger@2.3-impl \
+    android.hardware.audio.sounddose-vendor-impl
 
 PRODUCT_PACKAGES += \
     audioadsprpcd \
