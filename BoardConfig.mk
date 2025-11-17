e6a4e6cd45e744c224aead1bcf8efe4f5231b8b1
Alexander Koskovich <akoskovich@pm.me>
2025-11-17 10:06:45 +0000

    peridot: Remove virtual framebuffer
    * It's largely for testing purposes and it consumes
       a large amount of memory. If your hardware doesn't
       have graphics then it can be useful, which isn't the
       case for shipped devices.
    
    Change-Id: I2348718ad3afba846498572161fa7e83dd8b7ce9
    Signed-off-by: Guidix <guidixm@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -125,7 +125,6 @@ TARGET_KERNEL_CONFIG := \
     vendor/peridot_GKI.config
 
 BOARD_KERNEL_CMDLINE := \
-    video=vfb:640x400,bpp=32,memsize=3072000 \
     swinfo.fingerprint=peridot:$(VOLTAGE_VERSION) \
     mtdoops.fingerprint=peridot:$(VOLTAGE_VERSION)
 
