7177de2c07234a9b8cf465507ea857cd77292ca2
Guidix <guidixm@gmail.com>
2025-12-30 21:29:53 +0200

    peridot: Squashed revert of torch light control
    Reason: We have native support for it
    
    Revert "peridot: camera: Add enabled bool for finished workarounds"
    
    This reverts commit 4a61cc89852e5f410b183e4360a743936bcfbd2f.
    
    Revert "peridot: camera: Implement setTorchModeExt"
    
    This reverts commit 7c9460e28de34bbce76d890dbe430cefe5b90e0c.
    
    Revert "peridot: camera: Implement supportsSetTorchModeExt"
    
    This reverts commit bfdc7f3a225fbd0ea571e8f7d227369d8df7bd55.
    
    Revert "peridot: Implement torch light control"
    
    This reverts commit 189547ad154dd2f587a75df691c81bdb631d4684.
    
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- device.mk
+++ device.mk
@@ -141,9 +141,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
-# Camera Extensions
-$(call soong_config_set,libcameraservice,ext_lib,//$(LOCAL_PATH):libcameraservice_extension.peridot)
-
+# Camera Extensions permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/camerax-vendor-extensions.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/camerax-vendor-extensions.xml
 
